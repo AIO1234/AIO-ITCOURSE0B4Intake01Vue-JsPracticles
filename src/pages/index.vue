d2b6de4962b7ed99da546af8e7eb51019365abd0
Yasindu <yasindu@sp-solutions.biz>
2025-08-30 20:43:58 +0530

    updated lifecyle hook

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,26 +6,32 @@
 export default {
   components: {},
 
+  // this occures before creating the component
   beforeCreate() {
     console.log("before create");
   },
 
+  // this occures after created the component
   created() {
     console.log("created");
   },
 
+  // this occures before loding the component
   beforeMount() {
     console.log("before mount");
   },
 
+  // this occures after loaded the component
   mounted() {
     console.log("mounted");
   },
 
+  // this occures before update anything in the component(after loaded)
   beforeUpdate() {
     console.log("before update");
   },
 
+  // this occures after updated anything in the component
   updated() {
     console.log("updated");
   },
