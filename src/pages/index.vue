e4db2353b792c07a34a147ed25b9a8e2c2fd41eb
Yasindu <yasindu@sp-solutions.biz>
2025-08-30 20:35:03 +0530

    updated

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,33 @@
 <template>
-  Hello
-  Hellooo
-  jek,jmhnbvcx,mnhb,kh 
-  jhgfdghlkjhgrf
-  ijtyrfdhjbgf
+  Hello Hellooo jek,jmhnbvcx,mnhb,kh jhgfdghlkjhgrf ijtyrfdhjbgf
 </template>
 
-<script >
-  //
+<script>
+export default {
+  components: {},
+
+  beforeCreate() {
+    console.log("before create");
+  },
+
+  created() {
+    console.log("created");
+  },
+
+  beforeMount() {
+    console.log("before mount");
+  },
+
+  mounted() {
+    console.log("mounted");
+  },
+
+  beforeUpdate() {
+    console.log("before update");
+  },
+
+  updated() {
+    console.log("updated");
+  },
+};
 </script>
--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <!-- test component one -->
-
+    <h1>Hello Test Component</h1>
+    Hello
     <div class="pt-16"></div>
     <TestComponentOne />
   </div>
